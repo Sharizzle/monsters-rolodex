741b33f838aca4c2cdde3998db9d63b149794fc8
Sharizzle <sharif@thekanaan.com>
2020-06-10 17:48:27 +0300

    Updates

---
+++ precache-manifest.3726306a40de0e32f803c8feecd78d8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ac3c6cc0d9bfd8ad90423d20e708226",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d1535c7bad743d8936a7",
+    "url": "/monsters-rolodex/static/css/main.da32003e.chunk.css"
+  },
+  {
+    "revision": "4ecb9f0df0474c1b1fa9",
+    "url": "/monsters-rolodex/static/js/2.47062208.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.47062208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1535c7bad743d8936a7",
+    "url": "/monsters-rolodex/static/js/main.67aaedbc.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
